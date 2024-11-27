3407c6bf6a963ecf0d2208ac1658eb2650626507
Audrey <crystale.trg@outlook.com>
2024-11-27 17:45:18 +0100

    feat: modified user repo to work with jakarta

--- src/main/java/fr/cyu/jee/service/UserRepository.java
+++ src/main/java/fr/cyu/jee/service/UserRepository.java
@@ -1,24 +1,38 @@
 package fr.cyu.jee.service;
 
 import fr.cyu.jee.model.User;
+import fr.cyu.jee.model.UserType;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.NoResultException;
 
+import java.util.List;
 import java.util.Optional;
 
 public class UserRepository extends JpaRepository<Integer, User> {
-
     public UserRepository(EntityManager em) {
         super(em, "users", User.class);
+    }
+
+    public Optional<User> findById(int id) {
+        try {
+            return Optional.of(
+                    getEntityManager()
+                            .createQuery("SELECT u FROM User u WHERE u.id = :id", User.class)
+                            .setParameter("id", id)
+                            .getSingleResult()
+            );
+        } catch (NoResultException e) {
+            return Optional.empty();
+        }
     }
 
     public Optional<User> findByEmail(String email) {
         try {
             return Optional.of(
                     getEntityManager()
-                    .createQuery("SELECT u FROM User u WHERE u.email = :email", User.class)
-                    .setParameter("email", email)
-                    .getSingleResult()
+                            .createQuery("SELECT u FROM User u WHERE u.email = :email", User.class)
+                            .setParameter("email", email)
+                            .getSingleResult()
             );
         } catch (NoResultException e) {
             return Optional.empty();
@@ -38,4 +52,26 @@ public class UserRepository extends JpaRepository<Integer, User> {
             return Optional.empty();
         }
     }
+
+    public <T extends User> Optional<T> findByEmailAndType(String email, UserType type) {
+        return (Optional<T>) findByEmail(email).filter(user -> user.getUserType() == type);
+    }
+
+
+    public List findAllByTypeOrdered(UserType type) {
+        return getEntityManager()
+                .createNativeQuery("SELECT * FROM users u WHERE u.dtype = :?1 ORDER BY u.id ASC", User.class)
+                .setParameter(1, type.getDtype())
+                .getResultList();
+    };
+
+
+    public List<User> findAllByOrderByIdAsc(){
+        return getEntityManager()
+                .createQuery("SELECT u FROM User u ORDER BY u.id ASC", User.class)
+                .getResultList();
+    };
 }
+
+
+
