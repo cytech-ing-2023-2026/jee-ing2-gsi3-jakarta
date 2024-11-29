f9ae0e6a761e7045acd26306c88d8070961c849d
LiliRoseGicquel <124388564+LiliRoseGicquel@users.noreply.github.com>
2024-11-29 09:15:53 +0100

    feat:  add CourseRepository (#2)

--- src/main/java/fr/cyu/jee/service/CourseRepository.java
+++ src/main/java/fr/cyu/jee/service/CourseRepository.java
@@ -2,10 +2,51 @@ package fr.cyu.jee.service;
 
 import fr.cyu.jee.model.Course;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 public class CourseRepository extends JpaRepository<Integer, Course> {
 
     public CourseRepository(EntityManager em) {
         super(em, "courses", Course.class);
+    }
+
+    @Transactional
+    public Set<Course> getStudentCourses(int studentId, LocalDate from, LocalDate to) {
+        try {
+            TypedQuery<Course> query = getEntityManager().createQuery(
+                    "SELECT c FROM Course c JOIN c.students s " +
+                            "WHERE s.id = :studentId AND c.beginDate BETWEEN :from AND :to",
+                    Course.class
+            );
+            query.setParameter("studentId", studentId);
+            query.setParameter("from", from);
+            query.setParameter("to", to);
+            return new HashSet<>(query.getResultList());
+        } catch (NoResultException e) {
+            return Set.of();
+        }
+    }
+
+    @Transactional
+    public Set<Course> getTeacherCourses(int teacherId, LocalDate from, LocalDate to) {
+        try {
+            TypedQuery<Course> query = getEntityManager().createQuery(
+                    "SELECT c FROM Course c " +
+                            "WHERE c.teacher.id = :teacherId AND c.beginDate BETWEEN :from AND :to",
+                    Course.class
+            );
+            query.setParameter("teacherId", teacherId);
+            query.setParameter("from", from);
+            query.setParameter("to", to);
+            return new HashSet<>(query.getResultList());
+        } catch (NoResultException e) {
+            return Set.of();
+        }
     }
 }
